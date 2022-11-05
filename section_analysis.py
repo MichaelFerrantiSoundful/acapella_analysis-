d7b8bb1edf1e3e2ce5a5a420fdc3bd755c5ee6aa
Michael Ferranti <111791181+MichaelFerrantiSoundful@users.noreply.github.com>
2022-11-04 20:11:29 -0400

    Delete section_analysis.py

--- section_analysis.py
+++
@@ -1,8 +0,0 @@
-import pychorus
-import os
-from pychorus import find_and_output_chorus
-
-# os.chdir("/Users/michaelferranti/PycharmProjects/vocal2.0/vocals")
-# clip_length = 25
-#chorus_start_sec = find_and_output_chorus("AU_EPV_Acapella05_Lead_Dry_156_A#m.wav", "chorustest.wav", clip_length)
-#print(chorus_start_sec)
